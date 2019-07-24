c81d862b11424dce572882a4b30cda53094abeab
Dominic Hui <dominichui112@gmail.com>
2019-07-24 10:48:23 +0100

    installed django rest framework 3.9.2, added to project settings, created api app added to settings, configured api urls at project level and app level

--- library_project/settings.py
+++ library_project/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'rest_framework',
+
     # local apps
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
+
+
 ]
 
 MIDDLEWARE = [
